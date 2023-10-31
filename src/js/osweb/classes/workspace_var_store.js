93ef1dfcdeca53389caec46f94f4146160fba7e3
smathot <s.mathot@cogsci.nl>
2023-10-31 09:46:52 +0100

    Automatically set URL query parameters as experimental variables - Both when set through JATOS or standalone - Closes #105

--- src/js/osweb/classes/workspace_var_store.js
+++ src/js/osweb/classes/workspace_var_store.js
@@ -8,6 +8,20 @@ export default class WorkspaceVarStore extends VarStore {
   constructor(item, parent = null) {
     super(item, parent)
     this._scope = window
+    if (typeof jatos !== 'undefined') {
+      // JATOS uses a special mechanism to pass query parameters
+      for (const param in jatos.urlQueryParameters) {
+        let val = jatos.urlQueryParameters[param]
+        console.log(`JATOS query parameter: ${param} = ${val}`)
+        this.set(param, item.syntax.convert_if_numeric(val))
+      }
+    } else {
+      // When running outside of JATOS we use the standard URL query parameters
+      const urlParams = new URLSearchParams(window.location.search)
+      for (const [param, val] of urlParams) {
+        console.log(`URL query parameter: ${param} = ${val}`)
+        this.set(param, item.syntax.convert_if_numeric(val))
+      }
+    }
   }
-
 }
